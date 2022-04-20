34c51d25626f1107022293a86dec5bd2df209fc8
Murilo Ribeiro <murilorsv14@gmail.com>
2022-04-20 15:10:12 -0300

    feat: requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    """ Permanence_period é um array de tuplas, enquanto target_time é o
+    horario a ser teste """
+
+    try:
+        casos_totais = 0
+        for tupla in permanence_period:
+            entrada_estudante = tupla[0]
+            saida_estudante = tupla[1]
+            if entrada_estudante <= target_time <= saida_estudante:
+                casos_totais += 1
+        return casos_totais
+    except (ValueError,IndexError, TypeError):
+            return None
