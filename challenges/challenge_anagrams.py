36e201990cae3969adb3dcf1abca056288c244b2
Murilo Ribeiro <murilorsv14@gmail.com>
2022-04-21 17:18:20 -0300

    compara dois arrays, e nao mais duas strings de joins de arrays

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,52 +1,25 @@
 def is_anagram(first_string, second_string):
     """ Algoritmos para encontrar anagramas com complexidade maxima O """
-    first_string_ordened = ordened_lower_string(first_string)
-    second_string_ordened = ordened_lower_string(second_string)
+    first_string_ordened = merge_sort(list(first_string.lower()))
+    second_string_ordened = merge_sort(list(second_string.lower()))
 
     return first_string_ordened == second_string_ordened
 
 
-def ordened_lower_string(string):
-    return "".join(merge_sort(string.lower()))
-
-
-def diferrent_lengths(word_A, word_B):
-    """ verifica se as palavras tem tamanhos diferentes """
-    if len(word_A) != len(word_B):
-        return True
-    return False
-
-
-def algoritmo_anagrama_complex_O_power_2(first_string, second_string):
-    """ Algoritmos para encontrar anagramas com complexidade O² """
-    second_string = second_string.lower()
-
-    if diferrent_lengths(first_string, second_string):
-        return False
-
-    for letter_in_first in first_string.lower():
-        if letter_in_first not in second_string:
-            return False
-        second_string = second_string.replace(letter_in_first, "")
-    return True
-
-
-def merge_sort(string):
-    copia_string_array = list(string)
-    array_of_letters = list(string)
+def merge_sort(array):
     # caso base: se já atingiu a menor porção (1)
-    if len(array_of_letters) <= 1:
+    if len(array) <= 1:
         # retorne o array
-        return array_of_letters
+        return array
     # calculo do pivot: índice que indica onde o array será particionado
     # no caso, metade
-    mid = len(array_of_letters) // 2
+    mid = len(array) // 2
     # para cada metade do array
     # chama a função merge_sort de forma recursiva
-    left, right = merge_sort(array_of_letters[:mid]), merge_sort(
-        array_of_letters[mid:])
+    left, right = merge_sort(array[:mid]), merge_sort(
+        array[mid:])
     # mistura as partes que foram divididas
-    return merge(left, right, copia_string_array)
+    return merge(left, right, array.copy())
 
 
 # função auxiliar que realiza a mistura dos dois arrays
@@ -80,3 +53,26 @@ def merge(left, right, merged):
         merged[left_cursor + right_cursor] = right[right_cursor]
 
     return merged
+
+
+def diferrent_lengths(word_A, word_B):
+    """ verifica se as palavras tem tamanhos diferentes """
+    if len(word_A) != len(word_B):
+        return True
+    return False
+
+
+def algoritmo_anagrama_complex_O_power_2(first_string, second_string):
+    """ Algoritmos para encontrar anagramas com complexidade O² """
+    second_string = second_string.lower()
+
+    if diferrent_lengths(first_string, second_string):
+        return False
+
+    for letter_in_first in first_string.lower():
+        if letter_in_first not in second_string:
+            return False
+        second_string = second_string.replace(letter_in_first, "")
+    return True
+
+# https://app.betrybe.com/course/computer-science/algoritmos/algoritmos-de-ordenacao-e-busca/29521083-44ea-488d-a74d-216b1ac79b04/conteudos/766d59bd-a04f-41a9-b41b-5fb37a2f2a3a/algoritmos-de-ordenacao/1adc1a6e-51c7-4065-ae2c-e5f8194e7578?use_case=side_bar
