001e3d70bfd6cc8fb565696c3164f29815a6f298
Murilo Ribeiro <murilorsv14@gmail.com>
2022-04-21 18:20:41 -0300

    redux complexy

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,13 +1,16 @@
 def find_duplicate(nums):
     """ Faça o código aqui. """
     try:
-        nums.sort()
-        for i in range(len(nums) - 1):
-            if nums[i] < 0:
-                return False
-            if nums[i] == nums[i+1]:
-                return nums[i]
-        return False
+        return duplicate_searches(nums)
     except TypeError:
         return False
-        
+
+
+def duplicate_searches(nums):
+    nums.sort()
+    for i in range(len(nums) - 1):
+        if nums[i] < 0:
+            return False
+        if nums[i] == nums[i+1]:
+            return nums[i]
+    return False
