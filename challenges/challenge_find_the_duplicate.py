2068cb2ad13894366e17aa8dd926c2cce7f2beed
Murilo Ribeiro <murilorsv14@gmail.com>
2022-04-21 18:16:50 -0300

    feat: find a duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,13 @@
 def find_duplicate(nums):
     """ Faça o código aqui. """
+    try:
+        nums.sort()
+        for i in range(len(nums) - 1):
+            if nums[i] < 0:
+                return False
+            if nums[i] == nums[i+1]:
+                return nums[i]
+        return False
+    except TypeError:
+        return False
+        
