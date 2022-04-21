5e13403fbfce679da423fe3f0a78ff48f838e739
Murilo Ribeiro <murilorsv14@gmail.com>
2022-04-21 12:33:33 -0300

    validar se a palavra é um palindromo

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,11 @@
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
+    for letter_in_first in second_string:
+        exist_letter = False
+        for letter_in_second in range:
+            if letter_in_first == letter_in_second:
+                exist_letter = True
+                second_string.remove(letter_in_second)
+        if exist_letter is False:
+            return False
+    return True
