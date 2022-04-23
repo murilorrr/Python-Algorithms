50c093c791530919a2871396c34cfd20c73e2fb5
Murilo Ribeiro <murilorsv14@gmail.com>
2022-04-23 11:30:48 -0300

    length optimization palindrome

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -10,7 +10,8 @@ def is_palindrome_iterative(word):
 
 
 def interative_palindrome(word):
-    for i in range(len(word) // 2):
-        if word[i] != word[len(word) - i - 1]:
+    length = len(word)
+    for i in range(length // 2):
+        if word[i] != word[length - i - 1]:
             return False
     return True
