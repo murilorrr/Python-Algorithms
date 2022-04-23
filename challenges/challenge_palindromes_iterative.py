9a96650069183f7b75a590f287f7ac180de86f29
Murilo Ribeiro <murilorsv14@gmail.com>
2022-04-23 10:53:07 -0300

    verify palindromes interative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,11 @@
 def is_palindrome_iterative(word):
     """ Faça o código aqui. """
+    if not word:
+        return False
+    try:
+        for i in range(len(word)):
+            if word[i] != word[len(word) - i - 1]:
+                return False
+        return True
+    except (TypeError, IndexError, ValueError):
+        return False
